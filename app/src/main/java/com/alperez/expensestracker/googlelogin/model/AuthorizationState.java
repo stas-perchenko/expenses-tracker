ebd245db135c6e40808cacb17f73f86dd3987aaf
Stanislav Perchenko <sperchenko@gmail.com>
2015-09-16 13:45:41 +0300

    :wq Please enter the commit message for your changes. Lines starting sdfsdfsdkjbfsdjk fskdjf sdf sdff

--- app/src/main/java/com/alperez/expensestracker/googlelogin/model/AuthorizationState.java
+++ app/src/main/java/com/alperez/expensestracker/googlelogin/model/AuthorizationState.java
@@ -4,5 +4,5 @@ package com.alperez.expensestracker.googlelogin.model;
  * Created by stanislav.perchenko on 15-Sep-15.
  */
 public enum AuthorizationState {
-    PICKING_ACCOUNT, AUTHORIZING, GETTING_TOKENS, GETTING_DATABASE,
+    PICKING_ACCOUNT, AUTHORIZING, GETTING_TOKENS, AUTHORIZED, AUTHORIZATION_FAILED,
 }
